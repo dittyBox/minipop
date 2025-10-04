669dab7d8dface252feb3a56df2356cb421bf9a2
ttp12345 <ttp12345@naver.com>
2025-10-05 00:11:07 +0900

    수정정

--- index.js
+++ index.js
@@ -5,4 +5,17 @@ window.addEventListener("load", (event) => {
     createBtn.addEventListener('click', (e) => {
       new minipop(parent, e.clientX, e.clientY, '이 팝업은 new로 생성됨');
     });
+    const testdataset = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+    const contents =  document.querySelector('.minipopGridcontainer');
+    testdataset.forEach(e=>{
+        contents.innerHTML = `${contents.innerHTML}
+        <div class="minipop-row">
+            <div><label class="checkbox-wrapper"><input type="checkbox"></label></div>
+            <div>데이터${e}-1</div>
+            <div>데이터${e}-2</div>
+            <div>데이터${e}-3</div>
+        </div>
+`
+    })
   });
